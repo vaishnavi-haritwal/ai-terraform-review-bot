80e08135bee78b0ed6f736fb5c93d5e85765314a
vaishnavi-haritwal <105439948+vaishnavi-haritwal@users.noreply.github.com>
2026-01-09 11:18:06 +0530

    Create insecure.tf

---
+++ examples/insecure.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "bad_example" {
+  ami           = "ami-123456"
+  instance_type = "t3.large"
+
+  tags = {
+    Name = "bad-instance"
+  }
+}
+
+resource "aws_security_group" "open_sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
